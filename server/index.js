177679b365bef350d829e4070184829fc2d82ef1
Sean Gorham <s.gorham.m@gmail.com>
2021-06-01 19:45:07 -0500

    Added connection from front end to backend and ability to create posts

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL ='mongodb+srv://crumbbum:shizzawn1123@cluster0.1sfwq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL ='mongodb+srv://crumbbum:S2gl44u!@cluster0.1sfwq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
